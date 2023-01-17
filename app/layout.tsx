e47b3b585b1337163f5a8d19fbb2345d51591a94
Sahinur Islam <infosahinur@gmail.com>
2023-01-17 22:41:44 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import './../styles/globals.css'
+import "./../styles/globals.css";
+import Header from "./../components/Header";
+import Footer from "./../components/Footer";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
