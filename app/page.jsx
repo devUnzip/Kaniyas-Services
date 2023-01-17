043b54ab45605b499c09e2de09681e5ab0c5fb9e
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-18 01:46:24 +0600

    footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Teams from '../components/Team/Teams'
 
 function page() {
   return (
-    <div className='text-4xl text-primary'>page</div>
+    <div className='text-4xl text-primary'>
+      <Teams></Teams>
+    </div>
   )
 }
 
