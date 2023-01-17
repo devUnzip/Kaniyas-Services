413523be6a3a26717dd46167ab6e79c38f56da49
Sahinur Islam <infosahinur@gmail.com>
2023-01-17 22:39:18 +0600

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='text-4xl text-primary'>page</div>
+    <div>contract</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8ea68e",
+        secondary: "#8e9f91"
+      }
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
   plugins: [],
-}
+};
